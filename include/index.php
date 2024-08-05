0d70a69c66a874cd960363c10d530894e924635c
Andres Elacion III <andreselacioniii@gmail.com>
2024-08-05 12:33:57 +0800

    php tutorials

---
+++ include/index.php
@@ -0,0 +1,20 @@
+<?php
+    include("../include/header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>This is the Home Page<br>
+    Stuff about your home page will go here</p><br>
+</body>
+</html>
+
+<?php
+    include("../include/footer.html");
+?>
